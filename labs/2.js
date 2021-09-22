99c209bbc0c062a6cccf7df3e1adb6fb35796db6
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-09-22 09:09:37 +0300

    Update 2.js

--- labs/2.js
+++ labs/2.js
@@ -15,3 +15,44 @@ function solve(str, char, result) {
         console.log('Not Matched');
     }
 }
+
+// 3
+function solve(first, second, third) {
+
+    let sum = first + second + third;
+    let type = undefined;
+
+    if (sum % 1 === 0) {
+        type = "Integer";
+    } else {
+        type = "Float";
+    }
+
+    console.log(`${sum} - ${type}`);
+}
+
+// Второ решение
+function solve(first, second, third) {
+
+    let sum = first + second + third;
+
+    sum % 1 === 0 ? sum += ' - Integer' : sum += ' - Float';
+    console.log(sum);
+}
+
+// 4
+function solve(number) {
+
+    let num = number.toString();
+    let sum = 0;
+
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+
+    let result = sum.toString().includes('9') ? `${num} Amazing? True` : `${num} Amazing? False`;
+
+    console.log(result);
+}
+
+// 5
