006c14dafc5d510b6eec7ada3efc43f75a01ef94
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-10-01 13:19:28 +0300

    Update 3.js

--- labs/3.js
+++ labs/3.js
@@ -68,3 +68,66 @@ function solve(arr) {
 
   console.log(sum);
 }
+
+// 6
+function solve(arr) {
+
+  let sumEven = 0;
+  let sumOdd = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Number(arr[i]);
+  }
+
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      sumEven += num;
+    } else {
+      sumOdd += num;
+    }
+  }
+
+  console.log(sumEven - sumOdd);
+}
+
+// 7
+function solve(firstArr, secondArr) {
+
+  let isIdentical = true;
+  let sumFirst = 0;
+  let sumSecond = 0;
+
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
+      console.log(`Arrays are not identical. Found difference at ${i} index`);
+      isIdentical = false;
+      break;
+    }
+  }
+
+  if (isIdentical) {
+    for (let numbers of firstArr) {
+      numbers = Number(numbers);
+      sumFirst += numbers;
+    }
+    
+    console.log(`Arrays are identical. Sum: ${sumFirst}`);
+  }
+}
+
+// 8
+function solve(nums) {
+
+  while (nums.length > 1) {
+    let condensed = [];
+    for (let i = 0; i < nums.length - 1; i++) {
+      let num1 = nums[i];
+      let num2 = nums[i + 1];
+      let sum = num1 + num2;
+      condensed.push(sum);
+    }
+    nums = condensed;
+  }
+
+  console.log(nums[0]);
+}
