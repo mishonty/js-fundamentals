0493fa4220f4c7d645b103a07dda8db3342d0886
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-10-01 10:49:19 +0300

    Update 3.js

--- labs/3.js
+++ labs/3.js
@@ -7,3 +7,64 @@ function solve(input) {
     console.log(first + last);
 }
 
+// 2
+function solve(day) {
+
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    day >= 1 && day <= 7 ? console.log(days[day-1]) : console.log("Invalid day!");
+}
+
+// 3
+function solve(n, arr) {
+
+  let newArr = [];
+  let output = '';
+
+  for (let i = 0; i < n; i++) {
+    newArr.push(arr[i]);
+  }
+
+  for (let i = newArr.length - 1; i >= 0; i--) {
+    output += `${newArr[i]} `;
+  }
+
+  console.log(output);
+}
+
+// 4
+function solve(arr) {
+
+  let revArr = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    revArr.push(arr[i]);
+  }
+
+  console.log(revArr.join(' '));
+}
+
+/////////////////////////////////////////////////////////////
+function reverse(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let buffer = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = buffer
+  }
+
+  console.log(arr.join(' '));
+}
+
+// 5
+function solve(arr) {
+
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Number(arr[i]) % 2 === 0) {
+      sum += Number(arr[i]);
+    }
+  }
+
+  console.log(sum);
+}
