d1ed57b2117fd183260fc717ca9557b2a3cbf6ad
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-10-08 11:08:22 +0300

    Update 4.js

--- labs/4.js
+++ labs/4.js
@@ -11,3 +11,31 @@ function solve(str, n) {
 }
 
 // 2
+function solve(grade) {
+
+  let result = '';
+  
+  if (grade < 3) {
+    result = 'Fail (2)';
+  } else if (grade >= 3 && grade < 3.5) {
+    result = `Poor (${grade.toFixed(2)})`;
+  } else if (grade >= 3.5 && grade < 4.5) {
+    result = `Good (${grade.toFixed(2)})`;
+  } else if (grade >= 4.5 && grade < 5.5) {
+    result = `Very good (${grade.toFixed(2)})`;
+  } else {
+    result = `Excellent (${grade.toFixed(2)})`;
+  }
+  
+  console.log(result);
+}
+
+// 3
+function solve(n, pow) {
+
+  let result = Math.pow(n, pow);
+
+  console.log(result);
+}
+
+// 4
