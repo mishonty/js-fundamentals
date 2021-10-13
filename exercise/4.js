62d69bf4bfe87901c380f3e340078d7097361693
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-10-13 17:00:16 +0300

    Update 4.js

--- exercise/4.js
+++ exercise/4.js
@@ -26,3 +26,76 @@ function solve(num1, num2, num3) {
 }
 
 // 3
+function solve(first, second) {
+
+    let firstSymbolAsNumber = first.charCodeAt(0);
+    let secondSymbolAsNumber = second.charCodeAt(0);
+    let bigger = Math.max(firstSymbolAsNumber, secondSymbolAsNumber);
+    let smaller = Math.min(firstSymbolAsNumber, secondSymbolAsNumber);
+    let output = '';
+
+    for (let i = smaller + 1; i < bigger; i++) {
+        let currentSymbolAsNumber = String.fromCharCode(i);
+        output += `${currentSymbolAsNumber} `;
+    }
+
+    console.log(output);
+}
+
+// 4
+function solve(num) {
+
+    let numbersToDo = num.toString();
+    let sumEven = 0;
+    let sumOdd = 0;
+
+    let takeSumEven = (numbersToDo) => {
+        for (let i = 0; i < numbersToDo.length; i++) {
+            let currentDigit = Number(numbersToDo[i]);
+
+            if (currentDigit % 2 === 0) {
+                sumEven += currentDigit;
+            }
+        }
+        return sumEven;
+    }
+
+    let takeSumOdd = (numbersToDo) => {
+        for (let i = 0; i < numbersToDo.length; i++) {
+            let currentDigit = Number(numbersToDo[i]);
+
+            if (currentDigit % 2 !== 0) {
+                sumOdd += currentDigit;
+            }
+        }
+        return sumOdd;
+    }
+
+    console.log(`Odd sum = ${takeSumOdd(numbersToDo)}, Even sum = ${takeSumEven(numbersToDo)}`);
+}
+
+// 5
+function solve(arr) {
+
+    let arrOfNumbers = arr;
+    let numbersL = arrOfNumbers.length;
+    let isPalindrome = (num) => {
+
+        let startNum = num;
+        let reversedNum = Number(num.toString().split('').reverse().join(''));
+
+        if (startNum === reversedNum) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    for (let i = 0; i < numbersL; i++) {
+        let currentNumber = arrOfNumbers[i];
+
+        console.log(isPalindrome(currentNumber));
+    }
+}
+
+// 6
