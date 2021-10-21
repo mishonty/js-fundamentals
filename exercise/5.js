b1aee4431ab55decf7e54c72db9a7a10460601b4
Mikhail <80749024+mishonty@users.noreply.github.com>
2021-10-21 09:42:36 +0300

    Update 5.js

--- exercise/5.js
+++ exercise/5.js
@@ -28,3 +28,47 @@ function solve(arr) {
 
   console.log(output);
 }
+
+
+
+
+
+function solve(arr) {
+
+  let wagoonsArr = arr
+    .shift()
+    .split(' ')
+    .map(Number);
+  let capacity = Number(arr.shift());
+  let wagoonsL = wagoonsArr.length;
+  let arrL = arr.length;
+  let output = [];
+
+  for (let i = 0; i < arrL; i++) {
+    let currentWagoon = wagoonsArr[i];
+    let currentElement = arr[i].split(' ');
+    let firstSign = currentElement[0];
+    let secondSign = currentElement[1];
+    let emptyseats = capacity - currentWagoon;
+
+    if (firstSign === 'Add') {
+      output.push(Number(secondSign));
+    }
+
+    if (emptyseats > Number(firstSign)) {
+      currentWagoon += Number(firstSign);
+      output.push(currentWagoon);
+    } else {
+      for (let j = 1; j < arrL; j++) {
+        currentWagoon = wagoonsArr[j];
+        if (emptyseats > Number(firstSign)) {
+        currentWagoon += Number(firstSign);
+        output.push(currentWagoon);
+        }
+      }
+    }
+  }
+
+console.log(wagoonsArr)
+  console.log(output);
+}
